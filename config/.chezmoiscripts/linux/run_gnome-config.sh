d1ad24da4a7a895a6021fb903d8bf5f7bd1f1a6c
vpletea <84437690+vpletea@users.noreply.github.com>
2025-08-01 17:13:40 +0300

    Update run_gnome-config.sh

--- config/.chezmoiscripts/linux/run_gnome-config.sh
+++ config/.chezmoiscripts/linux/run_gnome-config.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /run/current-system/sw/bin/dconf reset -f
-/run/current-system/sw/bin/dconf dconf load / < $HOME/.config/dconf/dconf.ini
+/run/current-system/sw/bin/dconf load / < $HOME/.config/dconf/dconf.ini
